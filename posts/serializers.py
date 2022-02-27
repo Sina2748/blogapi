eaa5a1898389b2c3c41e9877ca6d91b149cf4485
Sina Samii <sina.sam@gmail.com>
2022-02-27 05:06:32 +0330

    8

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,3 +1,5 @@
+# posts/serializers.py
+from django.contrib.auth import get_user_model # new
 from rest_framework import serializers
 from .models import Post
 
@@ -5,5 +7,12 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
-        fields = ('id', 'author', 'title', 'body', 'created_at',)
         model = Post
+        fields = ('id', 'author', 'title', 'body', 'created_at',)
+
+
+class UserSerializer(serializers.ModelSerializer): # new
+
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username',)
